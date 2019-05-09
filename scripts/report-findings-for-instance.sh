dc9e66b54ef18a1d1b95279ceeb368331b5b92ba
Paul Allen <pea1@cornell.edu>
2019-05-09 20:23:40 +0000

    rename script for clarity of purpose

--- scripts/report-findings-for-instance.sh
+++ scripts/report-findings-for-instance.sh
@@ -5,15 +5,16 @@
 # set -e
 # set -x
 
-INSTANCE_ID=i-09b8e69b75b4814cd
-CMD="aws ec2 describe-tags --filters Name=resource-id,Values=i-09b8e69b75b4814cd --query Tags[?Key==\`Name\`].Value --output text"
+INSTANCE_ID=${REPORT_FORMAT:-"i-09b8e69b75b4814cd"}
+ASSESSMENT_ARN=${ASSESSMENT_ARN:-"arn:aws:inspector:us-east-1:095493758574:target/0-upam1Mi6/template/0-nyZxudNI/run/0-5YvUT3oX"}
+CMD="aws ec2 describe-tags --filters Name=resource-id,Values=$INSTANCE_ID --query Tags[?Key==\`Name\`].Value --output text"
 
 INSTANCE_NAME=`$CMD`
 
 echo ====$INSTANCE_NAME========================================
 
 RESULT=`aws inspector list-findings \
-  --assessment-run-arns arn:aws:inspector:us-east-1:095493758574:target/0-upam1Mi6/template/0-nyZxudNI/run/0-5YvUT3oX \
+  --assessment-run-arns $ASSESSMENT_ARN \
   --filter agentIds=$INSTANCE_ID,severities=High \
   --query findingArns[] \
   --output text`
